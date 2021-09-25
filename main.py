e0c2d41ad0ca64b8bafd2701b8a4bfef6936eb45
achmang <63748733+achmang@users.noreply.github.com>
2021-09-25 10:21:37 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+
+path = "inputs/"
+
+def validate_input(file):
+    # only txt files are valid
+    if file.endswith(".txt"):
+        with open(path + file, "r") as f:
+            lines = f.readlines()
+            for i, line in enumerate(lines):
+            # the first 8 lines are just input values
+                if i >= 8:
+                    break
+        return True
+
+# def init_values(file):
+
+def map_to_array(file):
+    map_arr = []
+
+    with open(path + file, "r") as f:
+        lines = f.readlines()[8:]
+        for line in lines:
+            map_arr.append(line.split(' '))
+
+    return map_arr
+
+
+if __name__ == "__main__":
+    all_files = os.listdir(path)
+    print(all_files)
+    for file in all_files:
+        print(map_to_array(file))
