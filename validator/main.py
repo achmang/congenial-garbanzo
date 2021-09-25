a0106c182c296d6252bc06d18ad5c4561fd49f01
Thomas Acheson <t.acheson@reply.com>
2021-09-25 12:39:14 +0200

    added stuff lol

--- validator/main.py
+++ validator/main.py
@@ -14,17 +14,44 @@ def validate_input(file):
         return True
 
 def inputs_to_dict(file):
-
     input_dict = {
-
+        "map_size": {
+            "width":None,
+            "height":None,
+        },
+        "office_count":None,
+        "max_distance":None,
+        "services":None,
+        "building_costs":None,
+        "offices":[{
+            "x":None,
+            "y":None,
+            "points":None,
+        }]
     }
 
     with open(path + file, "r") as f:
-        # map starts at this part
-        lines = f.readlines()[:8]
-        for line in lines:
-            # remove the new line
-            line = line[:len(line)-1]
+        # input values begin here
+        lines = f.readlines()[0:8]
+        for i, line in enumerate(lines):
+            line = line.split()
+            if i == 0:
+                input_dict["map_size"]["width"] = int(line[0])
+                input_dict["map_size"]["height"] = int(line[1])
+                input_dict["office_count"] = int(line[2])
+                input_dict["max_distance"] = int(line[3])
+                input_dict["services"] = int(line[4])
+            if i == 1:
+                input_dict["building_costs"] = [int(string) for string in line]
+            if i >= 2 and i <= 2+input_dict:
+
+
+                
+
+
+
+        return input_dict
+
 
 def map_to_array(file):
     map_arr = []
@@ -41,6 +68,7 @@ def map_to_array(file):
 
 if __name__ == "__main__":
     all_files = os.listdir(path)
-    print(all_files)
     for file in all_files:
         print(map_to_array(file))
+        # print(inputs_to_dict(file))
+
