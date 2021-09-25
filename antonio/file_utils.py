bd097ee38e48a406578d4f12d443c2f7e45ad067
Antonio Delle Canne <Antonio@NETUK-836BL414.local>
2021-09-25 15:59:51 +0200

    added code

---
+++ antonio/file_utils.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+import sys
+from pathlib import Path
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import requests
+
+# from map_utils import *
+
+root = Path('./')
+input_path = root/'inputs'
+
+def map_to_graph(map):
+    '''
+    map: the map in numpy format
+    '''
+
+def find_areas(map):
+
